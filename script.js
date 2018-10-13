244c690082e56d06daa6d237502bd52434318567
Xinchro <TheXrocks@gmail.com>
2018-10-13 10:01:30 +0100

    improved timings and added front-loaded images to prevent flashing

--- script.js
+++ script.js
@@ -1,74 +1,185 @@
 let flashes = [
   {
     flash: "a",
-    flickerTime: 1000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
     flickerTime: 2000,
-    amount: 1,
-  },
-  {
-    flash: "b",
-    flickerTime: 1000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
-    flickerTime: 2000,
-    amount: 1,
-  },
-  {
-    flash: "ab",
-    flickerTime: 1000,
     amount: 5,
   },
   {
     flash: "pause",
     flickerTime: 4000,
     amount: 1,
+  },
+  {
+    flash: "v",
+    flickerTime: 2000,
+    amount: 5,
+  },
+  {
+    flash: "pause",
+    flickerTime: 4000,
+    amount: 1,
+  },
+  {
+    flash: "v",
+    flickerTime: 2000,
+    amount: 5,
+  },
+  {
+    flash: "pause",
+    flickerTime: 4000,
+    amount: 1,
+  },
+  {
+    flash: "av",
+    flickerTime: 2000,
+    amount: 5,
+  },
+  {
+    flash: "pause",
+    flickerTime: 8000,
+    amount: 1,
   }
 ]
 
-function getImage(name) {
+// we consider "true" for that light being ON
+let state = {
+  firstRun: true,
+  a: true,
+  v: true
+}
+
+function getImageResource(name) {
   switch(name) {
     case "a":
-      return "https://dummyimage.com/1920x1080/000/fff&text=a"
+      return "img/bwanaTVSignVOff.png"
       break
-    case "b":
-      return "https://dummyimage.com/1920x1080/000/fff&text=b"
+    case "v":
+      return "img/bwanaTVSignAOff.png"
       break
-    case "ab":
-      return "https://dummyimage.com/1920x1080/000/fff&text=ab"
+    case "av":
+      return "img/bwanaTVSignAVOff.png"
       break
     default:
-      return "https://dummyimage.com/1920x1080/000/fff&text=pause"
+      return "img/bwanaTVSign.png"
   }
 }
 
-let state = {
-  firstRun: true,
-  a: false,
-  b: false,
-  ab: false
+function toggleFlicker(name) {
+  if(name === "a") {
+    if(state.a) {
+      state.a = false
+    } else
+    if(state.v) {
+      state.a = true
+      state.v = true
+    } else
+    {
+      state.a = true
+    }
+  }
+  if(name === "v") {
+    if(state.a) {
+      state.a = true
+      state.v = true
+    } else
+    if(state.v) {
+      state.v = false
+    } else
+    {
+      state.v = true
+    }
+  }
+  if(name === "av") {
+    if(state.a) {
+      state.a = false
+      state.v = true
+    } else
+    if(state.v) {
+      state.a = true
+      state.v = false
+    } else
+    {
+      state.a = true
+      state.v = true
+    }
+  }
+}
+
+function getImage(state) {
+  // a+/a- a on/off
+  // v+/v- v on/off
+
+  if(state.a) {
+    if(state.v) {
+      // a+v+
+      return getImageResource("")
+    }
+    if(state.a && state.v) {
+      // a-v+
+      return getImageResource("v")
+    }
+    // a+v-
+    return getImageResource("a")
+  }
+  if(state.v) {
+    if(state.a) {
+      // a+v+
+      return getImageResource("")
+    }
+    if(state.a && state.v) {
+      // a+v-
+      return getImageResource("a")
+    }
+    // a-v+
+    return getImageResource("v")
+  }
+  if(state.a && state.v) {
+    if(state.a) {
+      // a-v+
+      return getImageResource("v")
+    }
+    if(state.v) {
+      // a+v-
+      return getImageResource("a")
+    }
+    // a+v+
+    return getImageResource("")
+  }
+  // both off
+  return getImageResource("av")
+}
+
+function setImage(imageURL) {
+  document.body.style.backgroundImage = `url('${imageURL}')`
 }
 
 function loop(index) {
+  // get the previous and next indexes
   let prevIndex = index-1 > -1 ? index-1 : flashes.length-1
   let nextIndex = index+1 < flashes.length ? index+1 : 0
 
   setTimeout(() => {
-    document.body.style.backgroundImage = `url('${getImage(flashes[index].flash)}')`
+    // do the flicker effect
+    toggleFlicker(flashes[index].flash)
 
+    // set the image to be displayed, based on what's in the state
+    setImage(getImage(state))
+
+    // debug
     console.log("----------------------")
+    console.log(state)
+    console.log(getImage(state))
     console.log("prev", prevIndex, flashes[prevIndex].flash, flashes[prevIndex].flickerTime)
     console.log("current", index, flashes[index].flash, flashes[index].flickerTime)
     console.log("next", nextIndex, flashes[nextIndex].flash, flashes[nextIndex].flickerTime)
 
+    // no longer the first run
     state.firstRun = false
+
+    // carry on with the loop
     loop(nextIndex)
-  }, state.firstRun ? 0 : flashes[prevIndex].flickerTime )
+  }, state.firstRun ? 0 : flashes[prevIndex].flickerTime) // if it's the first run, we don't want a blank image
 }
 
+// start loop
 loop(0)
