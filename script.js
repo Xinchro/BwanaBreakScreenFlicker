6510d17ff90c11c2b587a4b753860c03d11f4bf8
Xinchro <TheXrocks@gmail.com>
2018-10-16 00:27:29 +0100

    redid timings and pulled out sequence toa JSON file

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
-let flashes = [
-  {
-    flash: "a",
-    flickerTime: 2000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
-    flickerTime: 4000,
-    amount: 1,
-  },
-  {
-    flash: "v",
-    flickerTime: 2000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
-    flickerTime: 4000,
-    amount: 1,
-  },
-  {
-    flash: "v",
-    flickerTime: 2000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
-    flickerTime: 4000,
-    amount: 1,
-  },
-  {
-    flash: "av",
-    flickerTime: 2000,
-    amount: 5,
-  },
-  {
-    flash: "pause",
-    flickerTime: 8000,
-    amount: 1,
-  }
-]
+let flickers = []
 
-// we consider "true" for that light being ON
+// initial state, we consider "true" for that light being ON
 let state = {
   firstRun: true,
   a: true,
   v: true
 }
 
+/**
+  Gets the image file URI for the given string
+
+  @param {String} name - name of the light(s) we consider off
+  @return {String} the URI of the image
+*/
 function getImageResource(name) {
   switch(name) {
     case "a":
@@ -57,54 +22,38 @@ function getImageResource(name) {
       return "img/bwanaTVSignAOff.png"
       break
     case "av":
-      return "img/bwanaTVSignAVOff.png"
+      return "img/bwanaTVSign.png"
       break
     default:
-      return "img/bwanaTVSign.png"
+      return "img/bwanaTVSignAVOff.png"
   }
 }
 
+/**
+  Toggles the states of the lights - on/off
+
+  @param {String} name - name of the light(s) we're toggling
+  @returns {null}
+*/
 function toggleFlicker(name) {
   if(name === "a") {
-    if(state.a) {
-      state.a = false
-    } else
-    if(state.v) {
-      state.a = true
-      state.v = true
-    } else
-    {
-      state.a = true
-    }
+    state.a = !state.a
   }
   if(name === "v") {
-    if(state.a) {
-      state.a = true
-      state.v = true
-    } else
-    if(state.v) {
-      state.v = false
-    } else
-    {
-      state.v = true
-    }
+    state.v = !state.v
   }
   if(name === "av") {
-    if(state.a) {
-      state.a = false
-      state.v = true
-    } else
-    if(state.v) {
-      state.a = true
-      state.v = false
-    } else
-    {
-      state.a = true
-      state.v = true
-    }
+    state.a = !state.a
+    state.v = !state.v
   }
 }
 
+/**
+  Gets the correct image for the state we're in
+
+  @param {Object} state - the state object
+  @return {String} the correct image's URI
+*/
 function getImage(state) {
   // a+/a- a on/off
   // v+/v- v on/off
@@ -112,7 +61,7 @@ function getImage(state) {
   if(state.a) {
     if(state.v) {
       // a+v+
-      return getImageResource("")
+      return getImageResource("av")
     }
     if(state.a && state.v) {
       // a-v+
@@ -124,7 +73,7 @@ function getImage(state) {
   if(state.v) {
     if(state.a) {
       // a+v+
-      return getImageResource("")
+      return getImageResource("av")
     }
     if(state.a && state.v) {
       // a+v-
@@ -133,53 +82,69 @@ function getImage(state) {
     // a-v+
     return getImageResource("v")
   }
-  if(state.a && state.v) {
-    if(state.a) {
-      // a-v+
-      return getImageResource("v")
-    }
-    if(state.v) {
-      // a+v-
-      return getImageResource("a")
-    }
-    // a+v+
-    return getImageResource("")
-  }
   // both off
-  return getImageResource("av")
+  return getImageResource("")
 }
 
-function setImage(imageURL) {
-  document.body.style.backgroundImage = `url('${imageURL}')`
+/**
+  Sets the backgound image to the one at the given URI
+
+  @param {String} imageURL - the image URI
+  @return {null}
+*/
+function setImage(imageURI) {
+  document.body.style.backgroundImage = `url('${imageURI}')`
 }
 
-function loop(index) {
+let loopingInterval = setInterval(() => {})
+/**
+  The main loop
+
+  @param {Number} index - the index of where we want to start in the sequence
+  @param {Number} frame - optional, the frame of the flicker we're on, defaults to 0
+  @return {null}
+*/
+function loop(index, frame) {
   // get the previous and next indexes
-  let prevIndex = index-1 > -1 ? index-1 : flashes.length-1
-  let nextIndex = index+1 < flashes.length ? index+1 : 0
+  let prevIndex = index-1 > -1 ? index-1 : flickers.length-1
+  let nextIndex = index+1 < flickers.length ? index+1 : 0
+  let flicker = flickers[index]
+  if(frame === undefined) frame = 0
 
-  setTimeout(() => {
-    // do the flicker effect
-    toggleFlicker(flashes[index].flash)
+  let time = flicker.amount > 0 ? flicker.time/flicker.amount : flicker.time
 
-    // set the image to be displayed, based on what's in the state
+  clearInterval(loopingInterval)
+
+  if(state.firstRun) {
     setImage(getImage(state))
-
-    // debug
-    console.log("----------------------")
-    console.log(state)
-    console.log(getImage(state))
-    console.log("prev", prevIndex, flashes[prevIndex].flash, flashes[prevIndex].flickerTime)
-    console.log("current", index, flashes[index].flash, flashes[index].flickerTime)
-    console.log("next", nextIndex, flashes[nextIndex].flash, flashes[nextIndex].flickerTime)
-
-    // no longer the first run
     state.firstRun = false
+  }
 
-    // carry on with the loop
-    loop(nextIndex)
-  }, state.firstRun ? 0 : flashes[prevIndex].flickerTime) // if it's the first run, we don't want a blank image
+  loopingInterval = setInterval(() => {
+    // flicker the current frame on/off
+    toggleFlicker(flicker.name)
+
+    // set the image to be displayed, based on the current state
+    setImage(getImage(state))
+    // increment frame
+    frame++
+
+    if(flicker.time > time*frame) {
+      loop(index, frame)
+    } else {
+      loop(nextIndex)
+    }
+  }, time)
 }
 
-// start loop
-loop(0)
+// get the JSON file with the sequence data in
+const req = new XMLHttpRequest()
+req.open("GET", "flickers.json", true)
+req.onreadystatechange = function () {
+  if(!req.responseText) return
+  flickers = JSON.parse(req.responseText)
+  // start loop
+  loop(0)
+}
+req.send(null)
+
